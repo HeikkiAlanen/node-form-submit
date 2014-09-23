23c2227d6ccbb789aea9717561ed4647750a0bfa
Heikki Alanen <heikki.alanen@metropolia.fi>
2014-09-23 18:59:02 +0300

    Form read and sent to client.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var express = require('express');
+var fs = require('fs');
+
 var app = express();
 
+var txtHeader = fs.readFileSync('./views/globals/header.html', encoding='utf8');
+var txtForm = fs.readFileSync('./views/form.html', encoding='utf8');
+var txtFooter = fs.readFileSync('./views/globals/footer.html', encoding='utf8');
+
 app.get('/', function(req, res){
-  res.send('hello world');
+  res.send(txtHeader + txtForm + txtFooter);
 });
 
 app.listen(6500);
