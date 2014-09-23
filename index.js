4bf3d7667580bb726925b4f82e075373d1b592ba
Heikki Alanen <heikki.alanen@metropolia.fi>
2014-09-23 19:55:20 +0300

    POST handling added.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var fs = require('fs');
+var bodyParser = require('body-parser');
 
 var app = express();
 
@@ -7,8 +8,18 @@ var txtHeader = fs.readFileSync('./views/globals/header.html', encoding='utf8');
 var txtForm = fs.readFileSync('./views/form.html', encoding='utf8');
 var txtFooter = fs.readFileSync('./views/globals/footer.html', encoding='utf8');
 
+app.use(bodyParser.json())
+
 app.get('/', function(req, res){
   res.send(txtHeader + txtForm + txtFooter);
+});
+
+app.post('/', function (req, res) {
+  var email = req.params.email;
+  var name = req.params.name;
+  console.log(req.params);
+  console.log(name);
+  res.send("success");
 });
 
 app.listen(6500);
